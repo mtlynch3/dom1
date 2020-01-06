1864a9fd66a439a73042fda2ffd924c5992583bb
Suchwinder Singh <windersingh80@yahoo.com>
2020-01-06 14:06:25 -0500

    Assignment 3 questions

--- script.js
+++ script.js
@@ -1,4 +1,38 @@
 // 1 - Select the section with an id of container without using querySelector.
-document.getElementById("container");
+console.log(document.getElementById("container"));
 
-//2 
+//2
+console.log(document.querySelector("#container")); 
+
+//3
+console.log(document.querySelectorAll(".second"));
+
+//4 double check
+console.log(document.querySelector("ol .third"));
+
+//5 
+// document.getElementById("container").innerHTML = "Hello!";
+
+//6
+document.querySelector('.footer').classList.add('main');
+
+//7
+document.querySelector(".footer").classList.remove('main');
+
+//8
+var x = document.createElement("LI");
+
+//9
+var t = document.createTextNode("four");
+x.appendChild(t);
+
+//10
+document.querySelector("ul").appendChild(x);
+
+//11
+for( let i = 0; i < 3; i++){
+    document.querySelectorAll("ol li")[i].style.backgroundColor = "green";
+}
+//12
+document.querySelector(".footer").remove();
+
